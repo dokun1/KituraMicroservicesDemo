54f9ba2d4bd687416e8a173dab1498ee6ff5d91f
David Okun IBM <davidokunibm@Davids-MacBook-Pro.local>
2017-01-25 06:32:10 -0500

    Added client

---
+++ MobileClient/Kitura-Microservices-Client/Kitura-Microservices-Client/Animal.swift
@@ -0,0 +1,13 @@
+//
+//  Animal.swift
+//  Kitura-Microservices-Client
+//
+//  Created by David Okun IBM on 1/24/17.
+//  Copyright © 2017 David Okun IBM. All rights reserved.
+//
+
+import UIKit
+
+class Animal: NSObject {
+
+}
---
+++ MobileClient/Kitura-Microservices-Client/Kitura-Microservices-Client/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Kitura-Microservices-Client
+//
+//  Created by David Okun IBM on 1/24/17.
+//  Copyright © 2017 David Okun IBM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+
+}
+
